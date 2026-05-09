448cc426e5cb2a4b17301f518154673274fb8c94
Dev <devanshshukla957@gmail.com>
2026-05-10 04:08:20 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#This main.py file contains complete Python Programming Language from basic to advanced
+#The file is the part of our Qros AI Project
+
 print("HELLO QROS")
+
+
+print("This is Python")
+
